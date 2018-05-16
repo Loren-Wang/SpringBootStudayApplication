7675e84ebac316b1f816c5d5977ac70e2c7ede70
wangliang <745342832@qq.com>
2018-05-16 16:03:06 +0800

    初始化springboot学习项目

---
+++ src/main/java/com/springbootstuday/service/SpringBootStudayApplication.java
@@ -0,0 +1,12 @@
+package com.springbootstuday.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStudayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStudayApplication.class, args);
+	}
+}
