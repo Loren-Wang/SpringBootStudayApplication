5e15ac3aed01ea4f76496fff65b701d1db09be86
wangliang <745342832@qq.com>
2018-05-17 13:59:06 +0800

    修改测试文件

--- src/main/java/com/springbootstuday/service/Test/TestRequestController.java
+++ src/main/java/com/springbootstuday/service/Test/TestRequestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestRequestController {
 
-    @PostMapping
+    @PostMapping("/testtest")
     public String testTest(@RequestParam(name = "name",defaultValue = "宝宝我爱你，么么哒") String name){
         return "宝宝我爱你，么么哒";
     }
