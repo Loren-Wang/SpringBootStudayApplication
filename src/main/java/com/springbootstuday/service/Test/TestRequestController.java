19fa7768f8e8dcc2d98ea40fcc6736a59172431b
wangliang <745342832@qq.com>
2018-05-17 13:56:51 +0800

    新增测试文件

---
+++ src/main/java/com/springbootstuday/service/Test/TestRequestController.java
@@ -0,0 +1,14 @@
+package com.springbootstuday.service.Test;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestRequestController {
+
+    @PostMapping
+    public String testTest(@RequestParam(name = "name",defaultValue = "宝宝我爱你，么么哒") String name){
+        return "宝宝我爱你，么么哒";
+    }
+}
